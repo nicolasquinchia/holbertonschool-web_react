0d644f6280145bb2f049874b2349f1488de7685e
nicolasquinchia <nicolasquinchia@gmail.com>
2021-10-27 21:16:38 -0500

    Typescript, Namespace & Declaration merging

---
+++ 0x00-TypeScript/task_4/js/subjects/Cpp.ts
@@ -0,0 +1,20 @@
+/// <reference path="Teacher.ts" />
+/// <reference path="Subject.ts" />
+namespace Subjects {
+    export interface Teacher {
+        experienceTeachingC?: number;
+    }
+
+    export class Cpp extends Subject {
+        getRequirements(): string {
+            return "Here is the list of requirements for Cpp";
+        }
+        getAvailableTeacher(): string {
+            if (!this.teacher.experienceTeachingC) {
+                return "No available teacher";
+            } else {
+                return "Available Teacher: " + this.teacher.firstName;
+            }
+        }
+    }
+}

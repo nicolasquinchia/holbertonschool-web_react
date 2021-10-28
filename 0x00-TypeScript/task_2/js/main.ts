6d244cb7c9cf530a8e0ec6a487e3a1b8e49bcb04
nicolasquinchia <nicolasquinchia@gmail.com>
2021-10-27 21:11:43 -0500

    Typescript, Advanced Types, Logs and docs

--- 0x00-TypeScript/task_2/js/main.ts
+++ 0x00-TypeScript/task_2/js/main.ts
@@ -1,3 +1,8 @@
+/**
+ * Task 5
+ */
+console.log("Output Task 5");
+
 interface DirectorInterface {
     workFromHome(): string;
     getCoffeeBreak(): string;
@@ -7,7 +12,7 @@ interface DirectorInterface {
 interface TeacherInterface  {
     workFromHome(): string;
     getCoffeeBreak(): string;
-    workDirectorTasks(): string;
+    workTeacherTasks(): string;
 }
 
 class Director implements DirectorInterface {
@@ -29,7 +34,7 @@ class Teacher implements TeacherInterface {
     getCoffeeBreak(): string {
         return "Cannot have a break";
     }
-    workDirectorTasks(): string {
+    workTeacherTasks(): string {
         return "Getting to work";
     }
 }
@@ -45,3 +50,40 @@ function createEmployee(salary: number | string): Director | Teacher {
 console.log(createEmployee(200).getCoffeeBreak());
 console.log(createEmployee(1000).workFromHome());
 console.log(createEmployee('$500').getCoffeeBreak());
+
+/**
+ * Task 6
+ */
+console.log("Output Task 6");
+
+function isDirector(employee: DirectorInterface | TeacherInterface): employee is Director {
+    return employee.workFromHome() == "Working from home";
+}
+
+function executeWork(employee: DirectorInterface | TeacherInterface): string {
+    if (isDirector(employee)) {
+        return employee.workDirectorTasks();
+    } else {
+        return employee.workTeacherTasks();
+    }
+}
+
+console.log(executeWork(createEmployee(200)));
+console.log(executeWork(createEmployee(1000)));
+
+/**
+ * Task 7
+ */
+console.log("Output Task 7");
+
+type Subjects = "Math" | "History";
+function teachClass(todayClass: Subjects): string {
+    if (todayClass == "Math") {
+        return "Teaching Math";
+    } else if (todayClass == "History") {
+        return "Teaching History";
+    }
+}
+
+console.log(teachClass('Math'));
+console.log(teachClass('History'));
